8b797fc457407fb76f93c0ae26f211e81b296a8a
IanLuka0 <rianender0@gmail.com>
2026-09-25 17:02:07 -0300

    tela de cadastro
    Tela de cadastro concluida, feita com a mesma base e estrutura feita antes do site.
    Usando o package de react-router-dom e axios react-router-dom para redirecionar paar outra página ao clicar em certos botões.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import App from './App';
+import Cadastro from './Cadastro';
+import './index.css';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<App />} />
+        <Route path="/cadastro" element={<Cadastro />} />
+        <Route path="*" element={<Navigate to="/login" replace />} />
+      </Routes>
+    </BrowserRouter> 
+  </React.StrictMode>
+);
